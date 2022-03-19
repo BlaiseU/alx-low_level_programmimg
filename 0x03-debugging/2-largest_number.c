7553c6a55ffe8cd3c5a1924b7e0fdb07e2c192e7
blaisemary <blaseu8@gmail.com>
2022-03-19 04:59:39 +0000

    The C project debugging -

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "2-main.h"
+
+/**
+* main - prints the largest of 3 integers
+* Return: 0
+*/
+
+int main(void)
+int a;
+     int b;
+          int c;
+int largest_number(int a, int b, int c);
+        int largest;
+largest=largest_number(int a, int b, int c);
+      int  a = 972;
+      int  b = -98;
+      int  c = 0;
+{
+        
+
+        printf("%d is the largest number\n", largest);
+
+        return (0);
+}
